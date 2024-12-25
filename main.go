1f6420207a8162bd8e6c094c369a77d34006cdc8
Vlad <vlad@mac.lan>
2024-12-26 00:38:00 +0300

    Dealt with JSON conversion and added 1 hour forecast. Needs refactoring

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	api_call := "https://api.open-meteo.com/v1/forecast?latitude=55.7522&longitude=37.6156&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,weather_code,surface_pressure,wind_speed_10m&hourly=precipitation_probability,uv_index,is_day&wind_speed_unit=ms&timezone=Europe%2FMoscow&forecast_days=1"
+	api_call := "https://api.open-meteo.com/v1/forecast?latitude=55.7522&longitude=37.6156&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation_probability,precipitation,weather_code,surface_pressure,visibility,wind_speed_10m,temperature_80m,uv_index,is_day,sunshine_duration&wind_speed_unit=ms&timezone=Europe%2FMoscow&past_hours=1&forecast_days=1&forecast_hours=12"
 	resp, _ := http.Get(api_call)
 	body, _ := io.ReadAll(resp.Body)
 
@@ -18,12 +18,36 @@ func main() {
 		panic(err)
 	}
 
-	current := dat["current"].(map[string]interface{})
+	hourly := dat["hourly"].(map[string]interface{})
+
 	fmt.Printf(`Current temp: %v°C
 Feels like: %v°C
+Chance of rain/snow %v%%
 Wind speed: %vm/s
+Precipitation %vmm
+UV index %v
 `,
-		current["temperature_2m"],
-		current["apparent_temperature"],
-		current["wind_speed_10m"])
+		hourly["temperature_2m"].([]interface{})[1],
+		hourly["apparent_temperature"].([]interface{})[1],
+		hourly["precipitation_probability"].([]interface{})[1],
+		hourly["wind_speed_10m"].([]interface{})[1],
+		hourly["precipitation"].([]interface{})[1],
+		hourly["uv_index"].([]interface{})[1])
+
+	fmt.Printf(`
+In the next hour expect:
+
+Temp: %v°C
+Will feel like: %v°C
+Chance of rain/snow %v%%
+Wind speed: %vm/s
+Precipitation %vmm
+UV index %v
+`,
+		hourly["temperature_2m"].([]interface{})[2],
+		hourly["apparent_temperature"].([]interface{})[2],
+		hourly["precipitation_probability"].([]interface{})[2],
+		hourly["wind_speed_10m"].([]interface{})[2],
+		hourly["precipitation"].([]interface{})[2],
+		hourly["uv_index"].([]interface{})[2])
 }
